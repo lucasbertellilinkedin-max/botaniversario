bf5982c487fafcde6425cd429bc9fd7443396578
lucasbertellilinkedin-max <lucasbertellilinkedin@gmail.com>
2026-04-10 17:15:42 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,19 @@ console.log("🚀 Iniciando bot de aniversários...");
 
 const client = new Client({
   authStrategy: new LocalAuth(),
-  puppeteer: { headless: false }
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
+    ]
+  }
 });
 
 // QR CODE
